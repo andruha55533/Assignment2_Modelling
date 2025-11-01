332f65d4b8a3e6f58874c26b0208c196b42888ac
Andrii/Andrew <110409741+andruha55533@users.noreply.github.com>
2025-11-01 15:45:23 +0200

    Add validMountainArray function
    Implement function to validate mountain arrays.

---
+++ task8.js
@@ -0,0 +1,15 @@
+// 8. Mount.array
+function validMountainArray(arr) {
+  let n = arr.length;
+  if (n < 3) return false;
+
+  let i = 0;
+  while (i + 1 < n && arr[i] < arr[i + 1]) i++;
+  if (i === 0 || i === n - 1) return false;
+
+  while (i + 1 < n && arr[i] > arr[i + 1]) i++;
+  return i === n - 1;
+}
+
+console.log(validMountainArray([0,3,2,1])); 
+console.log(validMountainArray([3,5,5]));
