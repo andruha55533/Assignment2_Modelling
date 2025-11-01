ec9e0db6ba8d35f97ab05e0d7d6ddb950776d638
Andrii/Andrew <110409741+andruha55533@users.noreply.github.com>
2025-11-01 15:45:05 +0200

    Update task9.js

---
+++ task9.js
@@ -0,0 +1,12 @@
+// 9. biggest elem from the right
+function replaceElements(arr) {
+  let max = -1;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let temp = arr[i];
+    arr[i] = max;
+    if (temp > max) max = temp;
+  }
+  return arr;
+}
+
+console.log(replaceElements([17,18,5,4,6,1]));
